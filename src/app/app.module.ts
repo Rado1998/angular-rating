0a8b192fb54a1773efc4357121cacf518a215bbe
Radik Pogossian <radikpogossian@Radiks-MacBook-Pro.local>
2022-12-07 17:28:00 +0400

    added test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,23 @@ import { NgxStarsRatingModule } from 'ngx-stars-rating';
 
 import { AppComponent } from './app.component';
 import { RatingModule } from './rating-module/rating.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+// Import what you need. RECOMMENDED. ✔️
+import { StarOutline } from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [ StarOutline ];
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -12,9 +29,13 @@ import { RatingModule } from './rating-module/rating.module';
   imports: [
     BrowserModule,
     RatingModule,
-    NgxStarsRatingModule
+    NgxStarsRatingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzIconModule.forRoot([...icons])
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
